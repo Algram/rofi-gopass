7d8c9320e41df6c0d9c264ca876d1e5ba3f42da3
Algram <aliasgram@gmail.com>
2020-12-17 14:34:43 +0100

    chore(gopass): update gopass command for newest version

--- rofi-gopass.sh
+++ rofi-gopass.sh
@@ -4,4 +4,4 @@
 # It displays a list of available passwords defined with gopass
 # and types them once an item is selected with enter
 
-gopass ls --flat | rofi -dmenu -p gopass | xargs --no-run-if-empty gopass show -f | head -n 1 | xdotool type --clearmodifiers --file -
+gopass ls --flat | rofi -dmenu -p gopass | xargs --no-run-if-empty gopass show -o | head -n 1 | xdotool type --clearmodifiers --file -
